308a36c71e4aaeebfbb078a2c10fb578a46c9b93
David J. H. Shih <djh.shih@gmail.com>
2026-03-13 15:01:21 +0800

    Refactor.

--- gmail.ts
+++ gmail.ts
@@ -20,87 +20,14 @@ function onGmailMessage(e) {
 
 	var timeZone = CalendarApp.getTimeZone();
 
-	var body = message.getPlainBody();
-
-	// Remove possible email header
-	body = eatToken(body, /Subject: /, '\n');
-
-	// Remove return characters, which makes \n\n detection difficult
-	body = body.replace(/\r/g, '');
-
-	var title = parsePrefixedToken(body, /title\s*:/i, /\s*.+/);
-	if (title == '') {
-		title = subject;
-	}
-
-	// remove any parenthesized value (e.g. day of week)
-	var date = parsePrefixedToken(body, /date\s*:/i, /\s*[0-9A-Za-z ,./]+/i);
-	var time = parsePrefixedToken(body,
-		/time\s*:/i,
-		/\s*[0-9]+(:[0-9]+)?\s*((a|p)\.?m\.?)?\s*(-|–|(to))?\s*[0-9]+(:[0-9]+)?\s*((a|p)\.?m\.?)?/i
-	);
-
-	var location = parsePrefixedToken(body, /venue\s*:/i, /\s*.+/);
-	if (location == '') {
-		location = parsePrefixedToken(body, /location\s*:/i, /\s*.+/);
-	}
-
-	var error = '';
-	var description = '';
-	var startTime = '';
-	var endTime = '';
-
-	var i;
-
-	var biography = parsePrefixedParagraph(body, /^ *biography:?\s*$/im);
-	if (biography == '') {
-		biography = parsePrefixedParagraph(body, /^ *speakers?:?\s*$/im);
-	}
-
-	var abstract = parsePrefixedParagraph(body, /^ *abstract:?\s*$/im);
-	if (abstract == '') {
-		abstract = parsePrefixedParagraph(body, /^ *(seminar )?overview?:?\s*$/im);
-	}
-
-	if (biography != '') {
-		description += 'Biography:\n' + biography + '\n\n';
-	}
-
-	if (abstract != '') {
-		description += 'Abstract:\n' + abstract + '\n\n';
-	}
-
-	var times = parseTimeInterval(time);
-
-	// attempt to parse time
-	var startTimeObj = parseTime(times.start);
-	if (startTimeObj != null) {
-		times.start = Utilities.formatDate(startTimeObj, 'GMT', TIME_FORMAT);
-	}
-	var endTimeObj = parseTime(times.end);
-	if (endTimeObj != null) {
-		times.end = Utilities.formatDate(endTimeObj, 'GMT', TIME_FORMAT);
-	}
-
-	if (startTimeObj == null || endTimeObj == null) {
-		error += 'Error: Unrecognized time format. Please re-write in ' + TIME_FORMAT + ' format.\n';
-	}
-
-	date = normalizeDate(date);
-
-	// attempt to parse date here
-	var dateObj = parseDate(date);
-	if (dateObj != null) {
-		// standardize format
-		date = Utilities.formatDate(dateObj, 'GMT', DATE_FORMAT);
-	} else {
-		// add note that date failed to parse)
-		error += 'Error: Unrecognized date format. Please re-write in ' + DATE_FORMAT + ' format.\n';
+	var p = parseBody(message.getPlainBody());
+	if (p.title == '') {
+		p.title = subject;
 	}
 
 	return createCard(
 		newCalendarEvent(
-			title, date, timeZone, times.start, times.end, location, description, error
+			p.title, p.date, timeZone, p.times.start, p.times.end, p.location, p.description, p.error
 		)
 	);
 }
